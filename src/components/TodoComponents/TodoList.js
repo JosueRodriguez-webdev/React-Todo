681a8aff088a6f06733a8fe20bc0c247d2e253d2
Josue Rodriguez <jr2589213@gmail.com>
2020-02-09 16:46:20 -0800

    Initialize repository
    Initial commit, made a class component for Todo list, list creation.

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,24 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+import { render } from 'react-dom';
+
+class TodoList extends React.Component {
+    constructor() {
+        super();
+        this.state = [{
+            id: '',
+            note: '',
+            clearNote: false
+        }]
+    }
+
+    render() {
+        return(
+            <div>Hello World!</div>
+        )
+    }
+}
+
+export default TodoList;
